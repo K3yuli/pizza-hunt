23670e2c505b97963711b00d02a00f86f682bd61
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-03-02 19:34:56 -0500

    comment model-association

--- models/Pizza.js
+++ models/Pizza.js
@@ -2,7 +2,8 @@
 const { Schema, model } = require('mongoose');
 
 // create schema for model - client wants following data stored when users create a pizza:
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+    {
     pizzaName: {
         type: String
     },
@@ -19,7 +20,30 @@ const PizzaSchema = new Schema({
         default: 'Large'
     },
     // '[]' indicates array
-    toppings: []
+    toppings: [],
+
+    // association (in mongoose, w can instruct the parent to keep track of its children)
+    comments: [
+        {
+            // tell mongoose to expect an ObjectId
+            type: Schema.Types.ObjectId,
+            // tell it that its data comes from the Comment model
+            ref: 'Comment'
+        }
+    ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        // set id to false because this is a virtual that Mongoose returns, it's needed
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
 
 // create the Pizza model using the Pizza Schema
